7da4905bad314175553d1bfeabff468f6dc92772
oprony <oprony@uw.edu>
2022-03-07 13:51:21 -0800

    made template for app

--- ui.R
+++ ui.R
@@ -2,40 +2,67 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
-climate_data <- read.csv("the data")
+climate_data <- read.csv("")
 
 intro_tab <- tabPanel(
-  "Intro",
-  fluidPage(theme = bs_theme(bootswatch = "theme"),
-            p("Hi. This is an App")
+  "Introduction",
+  fluidPage(theme = bs_theme(bootswatch = "lumen"),
   )
 )
-
 
 plot_sidebar <- sidebarPanel(
   selectInput(
     inputId = "user_category",
-    label = "label",
-    choices = climate_data$country,
-    selected = "default",
+    label = 'label',
+    choices = c("choices", "choices"),
+    selected = "choices",
     multiple = TRUE)
 )
 
 plot_main <- mainPanel(
-  plotlyOutput(outputId = "plot")
+  plotlyOutput(outputId = "")
 )
 
-plot_tab <- tabPanel(
-  "Plot",
+chart_tab1 <- tabPanel(
+  "Chart 1",
   sidebarLayout(
     plot_sidebar,
     plot_main
-  )
+  ),
+  fluidPage(),
+
 )
 
+chart_tab2 <- tabPanel(
+  "Chart 2",
+  sidebarLayout(
+    plot_sidebar,
+    plot_main
+  ),
+  fluidPage(),
+  
+)
+
+chart_tab3 <- tabPanel(
+  "Chart 3",
+  sidebarLayout(
+    plot_sidebar,
+    plot_main
+  ),
+  fluidPage(),
+  
+)
+
+conclusion_tab <- tabPanel(
+  "Conclusion",
+  fluidPage()
+)
 
 ui <- navbarPage(
-  "title",
+  "Climate Data",
   intro_tab,
-  plot_tab
+  chart_tab1,
+  chart_tab2,
+  chart_tab3,
+  conclusion_tab
 )
