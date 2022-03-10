77db9c349aa1e3c7e832addad24a12da7d4b0cca
oprony <oprony@uw.edu>
2022-03-10 13:17:12 -0800

    final

--- app.R
+++ app.R
@@ -4,5 +4,6 @@ source("server.R")
 
 shinyApp(ui = ui, server = server)
 
-library(rsconnect)
-rsconnect::deployApp('C:\\Users\\owenp\\Documents\\Info201code\\final-project-cactusleather')
+# library(rsconnect)
+# laptop : rsconnect::deployApp('C:\\Users\\owenp\\Documents\\Info201code\\final-project-cactusleather')
+# desktop : rsconnect::deployApp('/Users/owenpronovost/Documents/final-project-cactusleather')
