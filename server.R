664f5fedb822499df316d8b6254a089840e35aba
sketchb02 <sketchb@uw.edu>
2022-03-10 02:18:00 -0800

    sketchb02's changes

--- server.R
+++ server.R
@@ -7,16 +7,21 @@ data <- read.csv("birds_arent_real_tweets.csv")
 server <- function(input, output) {
   
   output$plot_1 <- renderPlotly({
-    data <- data %>% filter(source %in% input$user_category_1)
-    data <- data %>% mutate(date = as.Date(strtrim(date, 10))) %>% group_by(date) %>% summarize(tweets = sum(n()), source = source)
+    data_1 <- data %>% filter(source %in% input$user_category_1)
+    data_1 <- data_1 %>% mutate(date = as.Date(strtrim(date, 10))) %>% group_by(date) %>% summarize(tweets = sum(n()), source = source)
     # Note: problem here with "tweets = sum(n())", it adds up all values and combines them into one single value used for the graph. Unsure how to fix this just yet.
-    my_plot <- ggplot(data = data) +
+    plot_1 <- ggplot(data = data_1) +
       geom_point(aes(x = date, y = tweets, color = source)) + 
       labs(title = "'Birds Aren't Real'-Related Tweets by Date", x = "Date", y = "Tweets", color = "Source") + 
       scale_x_date(breaks = as.Date(c("2021-12-09", "2021-12-16", "2021-12-23", "2021-12-30", "2022-01-06", "2022-01-13", "2022-01-20"))) + 
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-    my_plotly_plot <- ggplotly(my_plot) 
+    plotly_1 <- ggplotly(plot_1) 
     
-    return(my_plotly_plot)
+    return(plotly_1)
+  })
+  
+  output$plot_2 <- renderPlotly({
+    
+    
   })
 }
